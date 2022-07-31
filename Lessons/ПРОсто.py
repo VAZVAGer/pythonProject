77fad31bcbf5386517fcb86c5d73e82fea98118a
BYKOVI <vaz21081@ua.fm>
2022-07-31 16:27:44 +0300

    МАТриці

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,17 +1,9 @@
 # n, m = map(int, input().split())
-# cities = list(map(str, input().split()))
-cities = ['Москва', 'Астрахань', 'Новгород', 'Димитровград', 'Душанбе']
-initial = []
-final_letter = [111]
-for citie in cities:
-    initial.append(citie[0].lower())
-
-    if citie[-1].lower() == 'ь' or citie[-1].lower() == 'ъ' or citie[-1].lower() == 'ы':
-        final_letter.append(citie[-2].lower())
-    else:
-        final_letter.append(citie[-1].lower())
-final_letter.pop()
-if initial[1::] == final_letter[1::]:
-    print('ДА')
-else:
-    print("НЕТ")
+# words = list(map(str, input().split()))
+# lst_in = list(map(str.strip, sys.stdin.readlines()))
+n = int(input())
+matrix = [[1, 0, 0, 0, 0],
+          [0, 0, 1, 0, 1],
+          [0, 0, 0, 0, 0],
+          [0, 1, 0, 1, 0],
+          [0, 0, 0, 0, 0]]
