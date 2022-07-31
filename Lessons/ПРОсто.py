c2c7990f7ebafd4a5fb9ebcf7c73c104be825069
BYKOVI <vaz21081@ua.fm>
2022-07-31 17:36:18 +0300

    МАТриці

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,9 +1,19 @@
 # n, m = map(int, input().split())
 # words = list(map(str, input().split()))
 # lst_in = list(map(str.strip, sys.stdin.readlines()))
-n = int(input())
-matrix = [[1, 0, 0, 0, 0],
-          [0, 0, 1, 0, 1],
+matrix = [[0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0],
-          [0, 1, 0, 1, 0],
+          [0, 0, 1, 0, 0],
+          [0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0]]
+for ind_str, mea_str in enumerate(matrix):
+    for ind_line, mea_line in enumerate(mea_str):
+        if mea_line == 1:
+            print(f'{mea_line} [{ind_str}][{ind_line}]')
+            if mea_line[ind_str - 1][ind_line - 1] == 0 and mea_line[ind_str - 1][ind_line] == 0 and \
+                    mea_line[ind_str - 1][ind_line + 1] == 0 and mea_line[ind_str][ind_line - 1] == 0 and \
+                    mea_line[ind_str][ind_line + 1] == 0 and mea_line[ind_str + 1][ind_line - 1] == 0 and \
+                    mea_line[ind_str + 1][ind_line] == 0 and mea_line[ind_str + 1][ind_line + 1] == 0:
+                print('ДА')
+            else:
+                print('НЕТ')
