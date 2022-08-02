9fbc16e834ad8b1c40534efce70ce8d333add4b5
BYKOVI <vaz21081@ua.fm>
2022-08-02 19:46:18 +0300

    Поиск соседей РАБОТАЕТ!!!

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,73 +1,73 @@
 # n, m = map(int, input().split())
 # words = list(map(str, input().split()))
 # lst_in = list(map(str.strip, sys.stdin.readlines()))
-matrix = [[1, 1, 0, 0, 0],
+matrix = [[1, 0, 0, 0, 0],
+          [0, 0, 1, 0, 1],
           [0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0],
+          [0, 1, 0, 1, 0],
           [0, 0, 0, 0, 0]]
-answer_list = []
+list_of_neighbors = []
 for ind_str, mea_str in enumerate(matrix):
+
     for ind_line, mea_line in enumerate(mea_str):
         if mea_line == 1:
             try:
-                if matrix[ind_str - 1][ind_line - 1] == 1 and ind_str - 1 > 0 and ind_line - 1 > 0:
-                    answer_list.append("НЕТ")
+                if ind_str - 1 == -1 or ind_line - 1 == -1:
+                    pass
                 else:
-                    answer_list.append("ДА")
+                    list_of_neighbors.append(matrix[ind_str - 1][ind_line - 1])
             except:
                 pass
             try:
-                if matrix[ind_str - 1][ind_line] == 1 and ind_str - 1 > 0 and ind_line - 1 > 0:
-                    answer_list.append("НЕТ")
+                if ind_str - 1 == -1 or ind_line == -1:
+                    pass
                 else:
-                    answer_list.append("ДА")
+                    list_of_neighbors.append(matrix[ind_str - 1][ind_line])
             except:
                 pass
             try:
-                if matrix[ind_str - 1][ind_line + 1] == 1 and ind_str - 1 > 0 and ind_line - 1 > 0:
-                    answer_list.append("НЕТ")
+                if ind_str - 1 == -1 or ind_line + 1 == -1:
+                    pass
                 else:
-                    answer_list.append("ДА")
+                    list_of_neighbors.append(matrix[ind_str - 1][ind_line + 1])
             except:
                 pass
             try:
-                if matrix[ind_str][ind_line - 1] == 1 and ind_str - 1 > 0 and ind_line - 1 > 0:
-                    answer_list.append("НЕТ")
+                if ind_str == -1 or ind_line - 1 == -1:
+                    pass
                 else:
-                    answer_list.append("ДА")
+                    list_of_neighbors.append(matrix[ind_str][ind_line - 1])
             except:
                 pass
             try:
-                if matrix[ind_str][ind_line + 1] == 1 and ind_str - 1 > 0 and ind_line - 1 > 0:
-                    answer_list.append("НЕТ")
+                if ind_str == -1 or ind_line + 1 == -1:
+                    pass
                 else:
-                    answer_list.append("ДА")
+                    list_of_neighbors.append(matrix[ind_str][ind_line + 1])
             except:
                 pass
             try:
-                if matrix[ind_str + 1][ind_line - 1] == 1 and ind_str - 1 > 0 and ind_line - 1 > 0:
-                    answer_list.append("НЕТ")
+                if ind_str + 1 == -1 or ind_line - 1 == -1:
+                    pass
                 else:
-                    answer_list.append("ДА")
+                    list_of_neighbors.append(matrix[ind_str + 1][ind_line - 1])
             except:
                 pass
             try:
-                if matrix[ind_str + 1][ind_line] == 1 and ind_str - 1 > 0 and ind_line - 1 > 0:
-                    answer_list.append("НЕТ")
+                if ind_str + 1 == -1 or ind_line == -1:
+                    pass
                 else:
-                    answer_list.append("ДА")
+                    list_of_neighbors.append(matrix[ind_str + 1][ind_line])
             except:
                 pass
             try:
-                if matrix[ind_str + 1][ind_line + 1] == 1 and ind_str - 1 > 0 and ind_line - 1 > 0:
-                    answer_list.append("НЕТ")
+                if ind_str + 1 == -1 or ind_line + 1 == -1:
+                    pass
                 else:
-                    answer_list.append("ДА")
+                    list_of_neighbors.append(matrix[ind_str + 1][ind_line + 1])
             except:
                 pass
-
-yes_or_not = "НЕТ" in answer_list
+yes_or_not = 1 in list_of_neighbors
 if yes_or_not == False:
     print("ДА")
 else:
