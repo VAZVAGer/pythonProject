32846c9f141017d3bd582c075c1f32d1527f6cf0
BYKOVI <vaz21081@ua.fm>
2022-08-02 10:29:44 +0300

    не верно ищет соседей, у ходит в отрицательный индекс

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -2,15 +2,14 @@
 # words = list(map(str, input().split()))
 # lst_in = list(map(str.strip, sys.stdin.readlines()))
 matrix = [[1, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0],
+          [0, 0, 0, 1, 0],
+          [0, 0, 1, 0, 0],
           [0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0]]
 answer_list = []
 for ind_str, mea_str in enumerate(matrix):
     for ind_line, mea_line in enumerate(mea_str):
         if mea_line == 1:
-            print(mea_line)
             try:
                 if matrix[ind_str - 1][ind_line - 1] == 1:
                     answer_list.append("НЕТ")
@@ -68,4 +67,9 @@ for ind_str, mea_str in enumerate(matrix):
             except:
                 pass
 
-print(answer_list)
+
+yes_or_not = "НЕТ" in answer_list
+if yes_or_not == False:
+    print("ДА")
+else:
+    print("НЕТ")
