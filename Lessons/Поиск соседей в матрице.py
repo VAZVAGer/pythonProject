8eebe737f19eaefc6224c0db7d55eaddd8532ded
BYKOVI <vaz21081@ua.fm>
2022-08-02 14:33:09 +0300

    попрежнему не работает корректно по краям матрицы

--- Lessons/Поиск соседей в матрице.py
+++ Lessons/Поиск соседей в матрице.py
@@ -1,6 +1,3 @@
-# n, m = map(int, input().split())
-# words = list(map(str, input().split()))
-# lst_in = list(map(str.strip, sys.stdin.readlines()))
 matrix = [[1, 1, 0, 0, 0],
           [0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0],
