38ea944c269fe1129a8cbedfa101dd37987af31b
BYKOVI <vaz21081@ua.fm>
2022-08-02 10:17:17 +0300

    Осталось корректно выводить ответ.

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,19 +1,71 @@
 # n, m = map(int, input().split())
 # words = list(map(str, input().split()))
 # lst_in = list(map(str.strip, sys.stdin.readlines()))
-matrix = [[0, 0, 0, 0, 0],
+matrix = [[1, 0, 0, 0, 0],
           [0, 0, 0, 0, 0],
-          [0, 0, 1, 0, 0],
+          [0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0]]
+answer_list = []
 for ind_str, mea_str in enumerate(matrix):
     for ind_line, mea_line in enumerate(mea_str):
         if mea_line == 1:
-            print(f'{mea_line} [{ind_str}][{ind_line}]')
-            if matrix[ind_str - 1][ind_line - 1] == 0 and matrix[ind_str - 1][ind_line] == 0 and \
-                    matrix[ind_str - 1][ind_line + 1] == 0 and matrix[ind_str][ind_line - 1] == 0 and \
-                    matrix[ind_str][ind_line + 1] == 0 and matrix[ind_str + 1][ind_line - 1] == 0 and \
-                    matrix[ind_str + 1][ind_line] == 0 and matrix[ind_str + 1][ind_line + 1] == 0:
-                print('ДА')
-            else:
-                print('НЕТ')
+            print(mea_line)
+            try:
+                if matrix[ind_str - 1][ind_line - 1] == 1:
+                    answer_list.append("НЕТ")
+                else:
+                    answer_list.append("ДА")
+            except:
+                pass
+            try:
+                if matrix[ind_str - 1][ind_line] == 1:
+                    answer_list.append("НЕТ")
+                else:
+                    answer_list.append("ДА")
+            except:
+                pass
+            try:
+                if matrix[ind_str - 1][ind_line + 1] == 1:
+                    answer_list.append("НЕТ")
+                else:
+                    answer_list.append("ДА")
+            except:
+                pass
+            try:
+                if matrix[ind_str][ind_line - 1] == 1:
+                    answer_list.append("НЕТ")
+                else:
+                    answer_list.append("ДА")
+            except:
+                pass
+            try:
+                if matrix[ind_str][ind_line + 1] == 1:
+                    answer_list.append("НЕТ")
+                else:
+                    answer_list.append("ДА")
+            except:
+                pass
+            try:
+                if matrix[ind_str + 1][ind_line - 1] == 1:
+                    answer_list.append("НЕТ")
+                else:
+                    answer_list.append("ДА")
+            except:
+                pass
+            try:
+                if matrix[ind_str + 1][ind_line] == 1:
+                    answer_list.append("НЕТ")
+                else:
+                    answer_list.append("ДА")
+            except:
+                pass
+            try:
+                if matrix[ind_str + 1][ind_line + 1] == 1:
+                    answer_list.append("НЕТ")
+                else:
+                    answer_list.append("ДА")
+            except:
+                pass
+
+print(answer_list)
