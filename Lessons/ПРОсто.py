4bc3913320887bfe0beb9ca880a6160f77c44e9e
BYKOVI <vaz21081@ua.fm>
2022-08-04 20:21:02 +0300

    Начинаем сортировать , но пока не ясно как это делать.

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,16 +1,17 @@
 # n, m = map(int, input().split())
 # words = list(map(str, input().split()))
 # lst_in = list(map(str.strip, sys.stdin.readlines()))
-matrix = [[2, 3, 4, 5, 6],
-          [3, 2, 7, 8, 9],
-          [4, 7, 2, 0, 4],
-          [5, 8, 0, 2, 1],
-          [6, 9, 4, 1, 2]]
-answer = ""
-for ind_str, mea_str in enumerate(matrix):
-    for ind_line, mea_line in enumerate(mea_str):
-        if matrix[ind_str][ind_line] == matrix[ind_line][ind_str]:
-            answer = 'ДА'
-        else:
-            answer ="НЕТ"
-print(answer)
+matrix = [8, 11, -53, 2, 10, 11]
+
+n = 0
+while n <= len(matrix):
+    mini = 0
+    maxi = 0
+    n += 1
+    for vel in matrix[n+1::]:
+        vel = mini
+        if mini > maxi:
+            mini, maxi = maxi, mini
+
+
+
