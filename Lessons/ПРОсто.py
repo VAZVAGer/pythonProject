7db0336f9439b88853e39be30df909c77c60fe39
BYKOVI <vaz21081@ua.fm>
2022-08-05 20:05:37 +0300

    Сортировка пока не работает ((

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,17 +1,12 @@
 # n, m = map(int, input().split())
 # words = list(map(str, input().split()))
 # lst_in = list(map(str.strip, sys.stdin.readlines()))
-matrix = [8, 11, -53, 2, 10, 11]
+matrix = [8, 11, -53, 2, 10, 12]
+mini = 0
+mini_id = 0
+for index_1, znachenie_1 in enumerate(matrix):
+    print('znachenie_1', znachenie_1)
 
-n = 0
-while n <= len(matrix):
-    mini = 0
-    maxi = 0
-    n += 1
-    for vel in matrix[n+1::]:
-        vel = mini
-        if mini > maxi:
-            mini, maxi = maxi, mini
-
-
+    for index_2, znachenie_2 in enumerate(matrix[index_1 : :]):
+        print("znachenie_2", znachenie_2)
 
