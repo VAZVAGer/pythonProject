147075c54fae07fcf6301c1a7a2389e8c8766000
BYKOVS <vaz21081@ua.fm>
2022-08-30 14:57:51 +0300

    Рекрсивная функиция Фибоначи работает

---
+++ Lessons/Рекурсивная цункция Фибоначчи.py
@@ -0,0 +1,13 @@
+def fib_rec(N, f=[]):
+    if N >1000:
+        N = 1000
+    if f == []:
+        f = [1, 1]
+        N = N - 2
+    f.append(f[-1] + f[-2])
+
+    if N != 1:
+        fib_rec(N - 1, f)
+    return f
+
+print(*fib_rec(2000))
