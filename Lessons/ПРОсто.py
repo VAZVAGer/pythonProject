156800d642bc53e4be66f99d1fd9734862477e0d
VAZVAGer <Vaz21081@ua.fm>
2022-09-03 21:41:58 +0300

    Задача 7,11,4вввввв

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -3,16 +3,23 @@
 # velue = list(map(int, input().split()))
 
 
-def fib_rec(N, f=[]):
-    if N >1000:
-        N = 1000
-    if f == []:
-        f = [1, 1]
-        N = N - 2
-    f.append(f[-1] + f[-2])
+def sort(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        d = {result[0][0]: result[1][0], result[0][1]: result[1][1], result[0][2]: result[1][2],
+             result[0][3]: result[1][3]}
+        return d
 
-    if N != 1:
-        fib_rec(N - 1, f)
-    return f
+    return wrapper
 
-print(*fib_rec(2000))
+
+@sort
+def get_list(s1, s2):
+    lst_in1 = list(map(str, s1.split()))
+    lst_in2 = list(map(str, s2.split()))
+    lst_in3 = [lst_in1, lst_in2]
+    return lst_in3
+
+
+d = get_list(input(), input())
+print(*sorted(d.items()))
