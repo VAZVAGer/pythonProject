3cf9f04689f8ae3f85a952b3a0ac532cca593077
VAZVAGer <Vaz21081@ua.fm>
2022-09-25 21:14:02 +0300

    9.6.6 на пол пути

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -2,13 +2,18 @@
 # a, b = map(int, input().split())
 
 
-
 # считывание списка из входного потока
 lst_in = ['смартфон:120000', 'яблоко:2', 'сумка:560', 'брюки:2500', 'линейка:10', 'бумага:500']
 di = {}
 for zn in lst_in:
     lst = zn.split(":")
     di[int(lst[-1])] = lst[0]
-
 print(di)
 
+
+def cheap(dict):
+    ds = sorted(dict.items(), reverse=False)
+    print(ds)
+
+
+cheap(di)
