3e3a28781f8ce33cd4f5bfb0522cf2d914b46991
EVGEN <vaz21081@ua.fm>
2022-09-25 20:29:27 +0300

    Стер основную ветку, практикую ответвления

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,13 +1,3 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
-s = 'Python дай мне силы пройти этот курс до конца!'
-N = []
-lst = []
-for n, i in enumerate(s):
-    if n < 10:
-        N.append(n)
-zi = zip(s, N)
-for i in zi:
-    lst.append(i)
-print(lst)
