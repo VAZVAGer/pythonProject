0fe42ce4ae551d567f4230e5f930f9c795fefd94
EVGEN <vaz21081@ua.fm>
2022-09-25 20:28:11 +0300

    9.6.5 Решена

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,13 +1,11 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
-s = 'Python дай мне силы пройти этот курс до конца!'
-N = []
-lst = []
-for n, i in enumerate(s):
-    if n < 10:
-        N.append(n)
-zi = zip(s, N)
-for i in zi:
-    lst.append(i)
-print(lst)
+list1 = list(map(int, input().split()))
+list2 = list(map(int, input().split()))
+list1.sort()
+list2.sort(reverse=True)
+zi = zip(list1, list2)
+for a, b in zi:
+    print(a + b, end=" ")
+
