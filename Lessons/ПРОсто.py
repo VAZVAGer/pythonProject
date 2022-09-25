54d86735a1ee2381e50992829de8e193b64227ae
EVGEN <vaz21081@ua.fm>
2022-09-25 20:46:41 +0300

    9.6.6 В процесе решения!

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,3 +1,12 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
+
+
+# считывание списка из входного потока
+lst_in = ['смартфон:120000', 'яблоко:2', 'сумка:560', 'брюки:2500', 'линейка:10', 'бумага:500']
+for zn in lst_in:
+    lst = zn.split(":")
+
+    print(lst)
+
