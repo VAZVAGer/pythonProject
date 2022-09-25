63c5ac8767decd81e3866cb8b9a08a7b32fb3394
EVGEN <vaz21081@ua.fm>
2022-09-25 20:16:24 +0300

    9.6.4 Решена

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,13 +1,10 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
-s = 'Python дай мне силы пройти этот курс до конца!'
-N = []
-lst = []
-for n, i in enumerate(s):
-    if n < 10:
-        N.append(n)
-zi = zip(s, N)
-for i in zi:
-    lst.append(i)
-print(lst)
+lst_in = list(map(int, input().split()))
+ss = set(lst_in)
+lst = list(ss)
+lst.sort(reverse=True)
+print(*lst[:4])
+
+
