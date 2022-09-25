eb0aa1ef154ed0e02a41dc99b9cebde91b1a62b4
EVGEN <vaz21081@ua.fm>
2022-09-25 19:20:31 +0300

    9.6 Решено

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,13 +1,10 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
-s = 'Python дай мне силы пройти этот курс до конца!'
-N = []
-lst = []
-for n, i in enumerate(s):
-    if n < 10:
-        N.append(n)
-zi = zip(s, N)
-for i in zi:
-    lst.append(i)
-print(lst)
+
+d = {'cat': 'кот', 'horse': 'лошадь', 'tree': 'дерево', 'dog': 'собака', 'book': 'книга'}
+def get_sort(d):
+    ds = dict(sorted(d.items(), reverse=True))
+    return [*ds.values()]
+
+get_sort(d)
