1f189ab5e35ccc868ad8cbb8252f9b6a53467489
EVGEN <vaz21081@ua.fm>
2022-09-25 17:25:30 +0300

    test1

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,39 +1,13 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
-from string import ascii_lowercase, ascii_uppercase
-import random
-
-N = int(input())
-
-
-def password_generator(number_of_digits):
-    chars = ascii_lowercase + ascii_uppercase + "0123456789!?@#$*"
-    N = 0
-    while N <= number_of_digits - 1:
-        random.seed()
-        indx = random.randint(0, len(chars) - 1)
-        N += 1
-        yield chars[indx]
-
-
-s = password_generator(N)
-for x in s:
-    print(x, end="")
-print()
-s = password_generator(N)
-for x in s:
-    print(x, end="")
-print()
-s = password_generator(N)
-for x in s:
-    print(x, end="")
-print()
-s = password_generator(N)
-for x in s:
-    print(x, end="")
-print()
-s = password_generator(N)
-for x in s:
-    print(x, end="")
-print()
+s = 'Python дай мне силы пройти этот курс до конца!'
+N = []
+lst = []
+for n, i in enumerate(s):
+    if n < 10:
+        N.append(n)
+zi = zip(s, N)
+for i in zi:
+    lst.append(i)
+print(lst)
