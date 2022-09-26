73d57ee50bf33ad750ec9b5686010bda50b614a6
EVGEN <vaz21081@ua.fm>
2022-09-26 20:07:43 +0300

    9.7.1 Решено!!

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,21 +1,6 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
-
-# считывание списка из входного потока
-lst_in = ['смартфон:120000', 'яблоко:2', 'сумка:560', 'брюки:2500', 'линейка:10', 'бумага:500']
-di = {}
-for zn in lst_in:
-    lst = zn.split(":")
-    di[int(lst[-1])] = lst[0]
-
-
-
-def cheap(dict):
-    ds = sorted(dict.items(), reverse=False)
-    sr = ds[:3]
-    for zn in sr:
-        print(zn[-1], end=" ")
-
-
-cheap(di)
+list1 = ['Лена', 'Енисей', 'Волга', 'Дон']
+s_list = sorted(list1, key=len, reverse=True )
+print(s_list)
