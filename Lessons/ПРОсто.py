8cc8504f379d79278973da041f2577e626fdf23a
EVGEN <vaz21081@ua.fm>
2022-09-28 21:05:41 +0300

    9,8,5 Решено

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,3 +1,9 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
+def get_list_dig(lst):
+    lst2 = []
+    for zn in lst:
+        if type(zn) in (int, float):
+            lst2.append(zn)
+    return lst2
