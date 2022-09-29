a148507fc38f2c57bc2ec549b0d7d97bc1ed0bb2
EVGEN <vaz21081@ua.fm>
2022-09-29 18:25:14 +0300

    9.9.3 Решено

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,3 +1,11 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
+def is_string(lst):
+    d = []
+    for zn in lst:
+        if type(zn) == str:
+            d.append(True)
+        else:
+            d.append(False)
+    return all(d)
