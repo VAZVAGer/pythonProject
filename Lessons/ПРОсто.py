f98e0dd0c2bcaadae123ffd10b80aa4733b24528
EVGEN <vaz21081@ua.fm>
2022-09-29 18:31:29 +0300

    9.9.4 Решено

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,3 +1,16 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
+list1 = list(map(int, input().split()))
+d = []
+for zn in list1:
+    if zn < 3:
+        d.append(False)
+    else:
+        d.append(True)
+
+rez = all(d)
+if rez == True:
+    print("учится")
+else:
+    print("отчислен")
