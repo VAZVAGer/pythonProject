87dec290341e25a2ec2f94ea2c2e1aba9bb8e021
EVGEN <vaz21081@ua.fm>
2022-09-29 18:14:51 +0300

    9.9.1 Решено

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,3 +1,13 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
+list1 = list(map(int, input().split()))
+list2 = []
+for zn in list1:
+    if zn % 2 == 0:
+        list2.append(True)
+    else:
+        list2.append(False)
+
+rezult = all(list2)
+print(rezult)
