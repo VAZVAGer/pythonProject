565696e90060618a3f8003ff4db5bf2196624aaa
EVGEN <vaz21081@ua.fm>
2022-09-29 18:19:05 +0300

    9.9.2 Решено

--- Lessons/ПРОсто.py
+++ Lessons/ПРОсто.py
@@ -1,3 +1,16 @@
 # list1 = list(map(int, input().split()))
 # a, b = map(int, input().split())
 
+list1 = list(map(float, input().split()))
+list2 = []
+for zn in list1:
+    if zn < 0:
+        list2.append(True)
+    else:
+        list2.append(False)
+
+rezult = any(list2)
+print(rezult)
+
+
+
