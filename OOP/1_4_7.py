831a5536915a5cc834b75124389e688ed2def759
EVGEN <vaz21081@ua.fm>
2022-10-22 18:56:34 +0300

    OOP 1_4_7 Решено

---
+++ OOP/1_4_7.py
@@ -0,0 +1,27 @@
+import sys
+
+
+class StreamData:
+    def create(self, fields, lst_values):
+
+        if len(fields) != len(lst_values):
+            return False
+        for i, key in enumerate(fields):
+            setattr(self, key, lst_values[i])
+        return True
+
+
+class StreamReader:
+    FIELDS = ('id', 'title', 'pages')
+
+    def readlines(self):
+        lst_in = list(map(str.strip, sys.stdin.readlines()))
+        #print(lst_in)# считывание списка строк из входного потока
+        sd = StreamData()
+        res = sd.create(self.FIELDS, lst_in)
+        print(res)
+        return sd, res
+
+
+sr = StreamReader()
+data, result = sr.readlines()
