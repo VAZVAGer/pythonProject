2fe3778be3ce2cb246e066f8fde79491b594bccb
EVGEN <vaz21081@ua.fm>
2022-10-23 20:09:25 +0300

    OOP 1_4_10 Надо победить повторное добавление русского слова

--- OOP/1_4_10.py
+++ OOP/1_4_10.py
@@ -1,7 +1,18 @@
 class Translator:
+    DICT = {}
     def add(self, eng, rus):
-        pass
-    def remove(self, eng):
-        pass
-    def translate(self, eng):
-        pass
+        self.eng = eng
+        self.rus = rus
+        if self.eng in self.DICT:
+            self.DICT["go"].append(self.rus)
+
+        else:
+            self.DICT[self.eng] = [self.rus]
+        print(self.DICT)
+
+
+ob_1 = Translator()
+ob_1.add("go", "идти")
+ob_1.add("go", "бег")
+ob_1.add("name", "имя")
+ob_1.add("go", "идти")
--- OOP/____.py
+++ OOP/____.py
@@ -1,9 +1,5 @@
-lst_in = ['1 Сергей 35 120000', '2 Федор 23 12000', '3 Иван 13 1200']
-FIELDS = ('id', 'name', 'old', 'salary')
-for velue in lst_in:
-    data = velue.split(" ")
-    dic = {}
-    for index, velue2 in enumerate(data):
-        dic[FIELDS[index]] = velue2
-    print(dic)
+dict_1 = {"go": ["идти"]}
+dict_1["go"].append("2222")
+
+print(dict_1["go"])
 
