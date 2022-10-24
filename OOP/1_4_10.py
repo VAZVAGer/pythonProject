1ce168cba336b797b2e5adc702f0d0256811f1a0
EVGEN <vaz21081@ua.fm>
2022-10-24 21:40:17 +0300

    OOP 1_4_10 Как бы готово, но последнюю проверку не проходит

--- OOP/1_4_10.py
+++ OOP/1_4_10.py
@@ -1,5 +1,6 @@
 class Translator:
     DICT = {}
+
     def add(self, eng, rus):
         self.eng = eng
         self.rus = rus
@@ -8,10 +9,25 @@ class Translator:
         if self.eng not in self.DICT:
             self.DICT[self.eng] = [self.rus]
 
+    def remove(self, eng):
+        self.eng = eng
+        self.DICT.pop(self.eng)
+
+    def translate(self, eng):
+        self.eng = eng
+        print(*self.DICT[self.eng])
 
 
-ob_1 = Translator()
-ob_1.add("go", "идти")
-ob_1.add("go", "бег")
-ob_1.add("name", "имя")
-ob_1.add("go", "идти")
+tr = Translator()
+tr.add("tree", "дерево")
+tr.add("car", "машина")
+tr.add("car", "автомобиль")
+tr.add("leaf", "лист")
+tr.add("river", "река")
+tr.add("go", "идти")
+tr.add("go", "ехать")
+tr.add("go", "ходить")
+tr.add("milk", "молоко")
+tr.remove("car")
+tr.translate("go")
+tr.translate("tree")[0]
