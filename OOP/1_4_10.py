3de863c7cc12bb3c5a7aebbb05102c3691e39ab6
EVGEN <vaz21081@ua.fm>
2022-10-24 21:00:06 +0300

    OOP 1_4_10 add горов

--- OOP/1_4_10.py
+++ OOP/1_4_10.py
@@ -3,12 +3,11 @@ class Translator:
     def add(self, eng, rus):
         self.eng = eng
         self.rus = rus
-        if self.eng in self.DICT:
-            self.DICT["go"].append(self.rus)
-
-        else:
+        if self.eng in self.DICT and self.rus not in self.DICT[self.eng]:
+            self.DICT[self.eng].append(self.rus)
+        if self.eng not in self.DICT:
             self.DICT[self.eng] = [self.rus]
-        print(self.DICT)
+
 
 
 ob_1 = Translator()
