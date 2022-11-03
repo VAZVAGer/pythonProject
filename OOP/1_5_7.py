4c77d75a0f28125be80b11c0428e49f5c86b0ea4
EVGEN <vaz21081@ua.fm>
2022-11-03 20:08:40 +0200

    OOP 1_5_7 в процессе

--- OOP/1_5_7.py
+++ OOP/1_5_7.py
@@ -1,7 +1,10 @@
 class CPU:
-    def __init__(self, name, clock_frequency):
+    def __init__(self, name, fr):
         self.name = name
-        self.tr = clock_frequency
+        self.tr = fr
+
+
+cp = CPU("INTEL", 16000)
 
 
 class Memory:
@@ -10,7 +13,22 @@ class Memory:
         self.volume = volume
 
 
+me1 = Memory("DDR4", 8)
+me2 = Memory("DDR4", 8)
+
+
 class MotherBoard:
-    def __init__(self, name, cpu, total_mem_slots=4, **mem_slots):
+    def __init__(self, name, cpu, **mem_slots):
         self.name = name
-        CPU = cpu
+        self.cpu = cpu
+        self.total_mem_slots = 4
+        self.mem_slots = mem_slots[:self.total_mem_slots]
+
+    def get_config(self):
+        return [f'Материнская плата: {self.name}',
+                f'Центральный процессор: {self.cpu}, {self.cpu.fr}',
+                f'Слотов памяти: {self.total_mem_slots}',
+                'Память:' + ';'.join(map(lambda x: f'{x.neme}- {x.volom}', self.mem_slots))]
+
+
+mb = MotherBoard("ASUS", cp, 4, me1, me2)
