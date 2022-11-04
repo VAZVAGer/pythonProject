5e7f178c2c5d636434f96298e6f6f5094dd7607c
EVGEN <vaz21081@ua.fm>
2022-11-04 20:39:52 +0200

    OOP 1_5_7 в процессеt

--- OOP/1_5_7.py
+++ OOP/1_5_7.py
@@ -4,31 +4,24 @@ class CPU:
         self.tr = fr
 
 
-cp = CPU("INTEL", 16000)
-
-
 class Memory:
     def __init__(self, name, volume):
         self.name = name
         self.volume = volume
 
 
-me1 = Memory("DDR4", 8)
-me2 = Memory("DDR4", 8)
-
-
 class MotherBoard:
-    def __init__(self, name, cpu, **mem_slots):
+    def __init__(self, name, cpu, *mems):
         self.name = name
         self.cpu = cpu
         self.total_mem_slots = 4
-        self.mem_slots = mem_slots[:self.total_mem_slots]
+        self.mem_slots = mems[:self.total_mem_slots]
 
     def get_config(self):
         return [f'Материнская плата: {self.name}',
-                f'Центральный процессор: {self.cpu}, {self.cpu.fr}',
+                f'Центральный процессор: {self.cpu.name}, {self.cpu.fr}',
                 f'Слотов памяти: {self.total_mem_slots}',
-                'Память:' + ';'.join(map(lambda x: f'{x.neme}- {x.volom}', self.mem_slots))]
+                'Память:' + ';'.join(map(lambda x: f'{x.neme} - {x.volom}', self.mem_slots))]
 
 
-mb = MotherBoard("ASUS", cp, 4, me1, me2)
+mb = MotherBoard("ASUS", CPU("Intel", 2000), Memory('kingston', 8), Memory('kingston', 8))
