ea568013cd43bb037d7ffd76d088dc797e29228f
EVGEN <vaz21081@ua.fm>
2022-11-04 21:20:03 +0200

    OOP 1_5_7 в процессе

---
+++ OOP/1_5_8.py
@@ -0,0 +1,39 @@
+class Cart:
+    def __init__(self):
+        self.goods = []
+
+    def add(self, gd):
+        self.goods.append(gd)
+
+    def remove(self, indx):
+        self.goods.remove(indx)
+
+    def get_list(self):
+        return ['<наименовние_1>: <цена_1>',
+                '<наименовние_2>: <цена_2>',
+                ...
+                '<наименовние_N>: <цена_N>']
+
+
+class Table:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class TV:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Notebook:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Cup:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
