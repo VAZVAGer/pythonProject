f501d6fe28529949daf5fb6f54c8982796ccbacb
EVGEN <vaz21081@ua.fm>
2022-11-07 20:22:19 +0200

    OOP 1_5_10 Впроцессе понимания что вообще надо сделать

---
+++ OOP/1_5_10.py
@@ -0,0 +1,11 @@
+class Cell:
+    def __init__(self, around_mines, mine):
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open = False  # False-клетка закрыта/True-открыта
+
+
+class GamePole:
+    def __init__(self,N, M):
+        self.N = N  # Размер поля
+        self.M = M  # Общее число мин на поле.
