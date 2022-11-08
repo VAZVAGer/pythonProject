3e3f76b61daee873ed99cdf2c09a78084c16481d
EVGEN <vaz21081@ua.fm>
2022-11-08 20:59:34 +0200

    OOP 1_5_10 создание пустого поля, готово

--- OOP/1_5_10.py
+++ OOP/1_5_10.py
@@ -1,5 +1,5 @@
 class Cell:
-    def __init__(self, around_mines, mine):
+    def __init__(self, around_mines=0, mine=False):
         self.around_mines = around_mines
         self.mine = mine
         self.fl_open = False  # False-клетка закрыта/True-открыта
@@ -10,11 +10,11 @@ class GamePole:
     def __init__(self, N, M):
         self.N = N  # Размер поля
         self.M = M  # Общее число мин на поле.
-        self.pole = [['#' for i in range(N)] for j in range(N)]  #Создание пустого поля N*N
+        self.pole = [[Cell() for i in range(N)] for j in range(N)]  #Создание пустого поля N*N
 
     def showe(self):
         print(self.pole)
 
-gp = GamePole(3, 0)
+gp = GamePole(2, 0)
 
 gp.showe()
