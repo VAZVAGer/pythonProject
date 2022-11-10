bd52ae097b61c3f261921c93e9006116b1310304
EVGEN <vaz21081@ua.fm>
2022-11-10 20:37:24 +0200

    OOP 1_5_10 пытаюсь раставить мины

--- OOP/1_5_10.py
+++ OOP/1_5_10.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Cell:
     def __init__(self, around_mines=0, mine=False):
         self.around_mines = around_mines
@@ -10,11 +13,23 @@ class GamePole:
     def __init__(self, N, M):
         self.N = N  # Размер поля
         self.M = M  # Общее число мин на поле.
-        self.pole = [[Cell() for i in range(N)] for j in range(N)]  #Создание пустого поля N*N
+        self.pole = [['#' for i in range(N)] for j in range(N)]  # Создание пустого поля N*N
+
+    def init(self):
+        object_list_mines = [True] * self.M
+        object_list = [False] * ((self.N * self.N) - self.M)
+        object_list = object_list + object_list_mines
+        random.shuffle(object_list)
+        for mi in object_list:
+            obj = Cell(0, mi)
 
     def showe(self):
         print(self.pole)
 
-gp = GamePole(2, 0)
+
+
+
+gp = GamePole(4, 0)
 
 gp.showe()
+
