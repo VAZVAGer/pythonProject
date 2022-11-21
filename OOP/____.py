ca36d46ee7c754bffea522a0b8b3f9bd148ccd03
EVGEN <vaz21081@ua.fm>
2022-11-21 19:08:33 +0200

    OOP 1_7_7 Надо проверить работает или нет

--- OOP/____.py
+++ OOP/____.py
@@ -1,23 +1,4 @@
-import random
-
-
-def list_mines_random(N, M):
-    object_list_mines = ['*'] * M
-    object_list = ['#'] * ((N * N) - M)
-    object_list = object_list + object_list_mines
-    random.shuffle(object_list)
-    print(object_list)
-    c = 0
-    B = [[0] * N for i in range(N)]
-    for i in range(N):
-        for j in range(N):
-            B[i][j] = object_list[c]
-            c += 1
-    print(B)
-
-
-
-list_mines_random(5, 5)
-
-
-
+from string import ascii_lowercase, digits
+CHARS = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя" + ascii_lowercase
+CHARS_CORRECT = CHARS + CHARS.upper() + digits
+print(CHARS_CORRECT)
