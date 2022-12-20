c75a3f2aeca44cd7104b77f89479df066271af7f
BUKOV <vaz21081@ua.fm>
2022-12-20 20:33:27 +0200

    OOP 1_7_8 в процессе

--- OOP/1_7_8.py
+++ OOP/1_7_8.py
@@ -1,11 +1,15 @@
 from string import ascii_lowercase, digits
+import re
 
 class CardCheck:
     CHARS_FOR_NAME = ascii_lowercase.upper() + digits
 
-def check_card_number(number):
-    if type(number) in int:
+    def check_card_number(number):
+        if type(number) in int:
+            if number == r'\d{4}-\d{4}-\d{4}-\d{4}' r'[A-Z]+\s[A-Z]+':
+                return True
+        else:
+            return False
 
-        return True
-    else:
-        return False
+    def check_name(name):
+#f'[{cls.CHARS_FOR_NAME}]+\s[{cls.CHARS_FOR_NAME}]+'
