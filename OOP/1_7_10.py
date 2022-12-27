3def447d4ac9de34e9f11f2a44a8283adfa89d28
BUKOV <vaz21081@ua.fm>
2022-12-27 20:43:09 +0200

    OOP 1_7_10 Start

---
+++ OOP/1_7_10.py
@@ -0,0 +1,10 @@
+class AppStore:
+    Application_list = []
+    def add_application(self, app):
+        pass
+    def remove_application(self, app):
+        pass
+    def block_application(self, app):
+        pass
+    def total_apps(self):
+        pass
