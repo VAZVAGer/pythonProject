3deaea395e0c11c190be4c451be5673c6cd5be44
BUKOV <vaz21081@ua.fm>
2022-12-27 21:25:38 +0200

    OOP 1_7_11 Начало

---
+++ OOP/1_7_11.py
@@ -0,0 +1,19 @@
+class Viber:
+    def __init__(self):
+        self.list_msg = []
+    def add_message(msg):
+        pass
+    def remove_message(msg):
+        pass
+    def set_like(msg):
+        pass
+
+    def show_last_message(число):
+        pass
+    def total_messages():
+        pass
+
+class Message:
+    def __init__(self, text, fl_like=False):
+        self.text = text
+        self.fl_like = fl_like
