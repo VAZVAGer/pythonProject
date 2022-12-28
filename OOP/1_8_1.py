5cbd0881e5db2bc43dce939f11c1b9b80e5b3fa8
BUKOV <vaz21081@ua.fm>
2022-12-28 21:20:54 +0200

    OOP 1_8 Начало2

--- OOP/1_8_1.py
+++ OOP/1_8_1.py
@@ -1,3 +1,19 @@
+class Router:
+    def __init__(self):
+        buffer = []
+        list_of_connected_servers = []
+
+    def link(self, server):  # Для присоеденения сервера к роутеру.
+        self.list_of_connected_servers.append(server)
+
+    def unlink(self, server):  # Для отсоединения сервера server (объекта класса Server) от роутера
+        self.list_of_connected_servers.remove(server)
+
+    def send_data(
+            self):  # Для отправки всех пакетов (объектов класса Data) из буфера роутера соответствующим серверам (после отправки буфер должен очищаться).
+        pass
+
+
 class Server:
     counter_server = 0
 
