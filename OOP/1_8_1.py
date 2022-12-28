ea4c3023852c72922ac82685ff2559375ec89d2b
BUKOV <vaz21081@ua.fm>
2022-12-28 21:07:24 +0200

    OOP 1_8 Начало1

--- OOP/1_8_1.py
+++ OOP/1_8_1.py
@@ -1,6 +1,13 @@
 class Server:
     counter_server = 0
+
     def __init__(self):
         self.buffer = []
         self.ip = self.counter_server + 1
         self.counter_server = self.ip
+
+
+class Data:
+    def __init__(self, data, ip):
+        self.data = data
+        self.ip = ip
