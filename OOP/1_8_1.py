b51a2d5437730a8f4fca1e45d623ed5e748ecdcf
BUKOV <vaz21081@ua.fm>
2023-01-03 21:48:50 +0200

    OOP 1_8 осталось сделать send_data, остальное вроде готово,

--- OOP/1_8_1.py
+++ OOP/1_8_1.py
@@ -9,20 +9,20 @@ class Router:
     def unlink(self, server):  # Для отсоединения сервера server (объекта класса Server) от роутера
         self.list_of_connected_servers.remove(server)
 
-    def send_data(
-            self):  # Для отправки всех пакетов (объектов класса Data) из буфера роутера соответствующим серверам (после отправки буфер должен очищаться).
-        for data in self.buffer:
-            msg = data.self.data
-            address = data.self.ip
+    def send_data(self):  # Для отправки всех пакетов (объектов класса Data) из буфера роутера соответствующим серверам (после отправки буфер должен очищаться).
+       for mes in self.buffer:
+           address_of_the_recipient = mes.Data.ip
+           information = mes.Data.data
 
 
-class Server: ## Вроде готов!
+class Server:  ## Вроде готов!
     counter_server = 0
 
     def __init__(self):
         self.buffer = []
         self.ip = Server.counter_server + 1
         Server.counter_server = Server.counter_server + 1
+
     def send_data(self, data, ip):
         Router().buffer.append(Data(data, ip))
 
@@ -32,12 +32,11 @@ class Server: ## Вроде готов!
     def get_ip(self):
         return self.ip
 
+
 class Data:  ## Класс готов.
     def __init__(self, data, ip):
         self.data = data
         self.ip = ip
-
-
 
 
 sv_from = Server()
