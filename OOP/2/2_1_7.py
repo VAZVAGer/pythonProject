ac483c3a429d35671e31b5fe353b514c0ed049bf
BUKOV <vaz21081@ua.fm>
2023-01-19 18:32:28 +0200

    OOP 2_1_7 Почемуто не работает

--- OOP/2/2_1_7.py
+++ OOP/2/2_1_7.py
@@ -8,10 +8,43 @@ class Point:
 
 
 class Rectangle:
-    def __init__(self, p1, p2):
-        self.__sp = p1
-        self.__ep = p2
 
-    def __init__(self, x1, y1, x2, y2):
-        self.__sp = Point(x1, y1)
-        self.__ep = Point(x2, y2)
+    def __init__(self, x1, y1, x2=None, y2=None):
+        if x2 == None and y2 == None:
+            self.__sp = x1
+            self.__ep = y2
+        else:
+            self.__sp = Point(x1, y1)
+            self.__ep = Point(x2, y2)
+
+    def set_coords(self, sp, ep):
+        self.__sp = sp
+        self.__ep = ep
+
+    def get_coords(self):
+        return self.__sp, self.__ep
+
+    def draw(self):
+        print(f"Прямоугольник с координатами: ({self.get_coords()}, {self.get_coords()}) ({self.get_coords()}, {self.get_coords()})")
+
+
+
+rect =  Rectangle(0, 0, 20, 34)
+
+
+assert isinstance(rect, Rectangle) and hasattr(Rectangle, 'set_coords') and hasattr(Rectangle, 'get_coords') and hasattr(Rectangle, 'draw'), "в классе Rectangle присутствуют не все методы"
+
+r = Rectangle(1, 2.6, 3.3, 4)
+r.set_coords(Point(1, 2), Point(3, 4))
+sp, ep = r.get_coords()
+a, b = sp.get_coords()
+c, d = ep.get_coords()
+assert a == 1 and b == 2 and c == 3 and d == 4, "метод get_coords вернул неверные значения координат"
+
+r = Rectangle(Point(1, 2), Point(3, 4))
+sp, ep = r.get_coords()
+a, b = sp.get_coords()
+c, d = ep.get_coords()
+assert a == 1 and b == 2 and c == 3 and d == 4, "метод get_coords вернул неверные значения координат"
+
+assert isinstance(r._Rectangle__sp, Point) and isinstance(r._Rectangle__ep, Point), "атрибуты __sp и __ep должны ссылаться на объекты класса Point"
