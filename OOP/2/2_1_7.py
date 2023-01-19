64630eb6c9058ce3d1b6e77be9cfb2684184f815
BUKOV <vaz21081@ua.fm>
2023-01-19 18:07:22 +0200

    OOP 2_1_7 Соджано два конструктора, один не пашет , надо как тоих ифнуть

--- OOP/2/2_1_7.py
+++ OOP/2/2_1_7.py
@@ -5,3 +5,13 @@ class Point:
 
     def get_coords(self):
         return self.__x, self.__y
+
+
+class Rectangle:
+    def __init__(self, p1, p2):
+        self.__sp = p1
+        self.__ep = p2
+
+    def __init__(self, x1, y1, x2, y2):
+        self.__sp = Point(x1, y1)
+        self.__ep = Point(x2, y2)
