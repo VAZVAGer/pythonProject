dd8cca1cacf5b3ca64644fd0a21d98dde8d5b51c
BUKOV <vaz21081@ua.fm>
2023-01-20 21:04:32 +0200

    OOP 2_1_10 Начало +1

--- OOP/2/2_1_10.py
+++ OOP/2/2_1_10.py
@@ -1,3 +1,16 @@
+from string import ascii_lowercase, digits
+import re
 class EmailValidator:
+    CHARS_FOR_NAME = ascii_lowercase.upper() + ascii_lowercase.lower() + digits + "_" + "."
     def __new__(cls, *args, **kwargs):
         return None
+
+    @classmethod
+    def get_random_email(cls):
+        pass
+
+    def __is_email_str(cls, email):
+        if type(email) == str:
+            return True
+        else:
+            return False
