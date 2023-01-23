a0e56d61cf95109c0db2c11a69049e5192eaa759
BUKOV <vaz21081@ua.fm>
2023-01-23 16:25:09 +0200

    OOP 2_1_10 valid_symbols!

--- OOP/2/2_1_10.py
+++ OOP/2/2_1_10.py
@@ -1,27 +1,44 @@
-
 import re
 import random
+
+
 class EmailValidator:
+    symbols = 'abcdefghijklmnopqrstuvwxyz0123456789_.@'
+
     def __new__(cls, *args, **kwargs):
         return None
 
     @classmethod
     def get_random_email(cls):
         random_value = random.randrange(1, 100)
-        random_mail = random.sample('abcdefghijklmnopqrstuvwxyz0123456789_.', random_value)
+        random_mail = random.sample(EmailValidator.symbols, random_value)
         random_mail = ''.join(random_mail)
         random_mail = random_mail + "@gmail.com"
         return random_mail
 
-    def __is_email_str(cls, email):
+    @staticmethod
+    def __is_email_str(email):
         if type(email) == str:
             return True
         else:
             return False
+
+    @staticmethod
+    def __valid_symbols(email):
+        list_valid = []
+        for symbol in email:
+            if EmailValidator.symbols.find(symbol) != -1:
+                list_valid.append(symbol)
+            if len(list_valid) == email:
+                return True
+            else:
+                return False
+
+
+
+    @classmethod
     def check_email(cls, email):
         if EmailValidator.__is_email_str():
-            pass
+
         else:
             return False
-
-
