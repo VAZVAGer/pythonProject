d984d651b8128a0af5091b509bf97e9ff7a2d275
BUKOV <vaz21081@ua.fm>
2023-02-07 20:55:28 +0200

    OOP 2_2_5 процессе.

---
+++ OOP/2/2_2_5.py
@@ -0,0 +1,26 @@
+class WindowDlg:
+    def __init__(self, title, width, heidht):
+        self.__title = title
+        self.__width = width
+        self.__height = heidht
+
+    def show(self):
+        print(f"{self.__title}: {self.__width}, {self.__height}")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, vel):
+        if type(vel) == int and 0 <= vel <= 10000:
+            self.__width = vel
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, vel):
+        if type(vel) == int and 0 <= vel <= 10000:
+            self.__height = vel
