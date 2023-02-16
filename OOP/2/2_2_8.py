f7c1f3f6bb05cccb73e1966cc5623f39b30ca504
BUKOV <vaz21081@ua.fm>
2023-02-16 20:27:20 +0200

    OOP 2_2_8 Что то налепил, но работает ли оно??

--- OOP/2/2_2_8.py
+++ OOP/2/2_2_8.py
@@ -1,27 +1,45 @@
 class TreeObj:
     def __init__(self, indx, value=None):
-        self.__indx = indx
-        self.__val = value
+        self.indx = indx
+        self.value = value
         self.__left = None
         self.__right = None
-
 
     @property
     def left(self):
         return self.__left
+
     @left.setter
-    def left(self,data):
+    def left(self, data):
         self.__left = data
 
     @property
-    def ridht(self):
+    def right(self):
         return self.__right
 
-    @ridht.setter
+    @right.setter
     def left(self, data):
         self.__right = data
 
 
+class DecisionTree:
+    def __init__(self):
+        self.head = None
+
+    @classmethod
+    def predict(cls, root, x):  ## x = [1, 1, 0]
+        for ind, vel in enumerate(x):
 
 
-
+    @classmethod
+    def add_obj(cls, obj, node=None, left=True):
+        if cls.head is None:
+            new_obj = obj
+            cls.head = new_obj
+            return new_obj
+        if left:
+            node.left = obj
+            return obj
+        if left == False:
+            node.ridht = obj
+            return obj
