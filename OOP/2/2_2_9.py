ae5990f85f31a9574caf9ede2ba2980118268fdf
BUKOV <vaz21081@ua.fm>
2023-03-26 20:40:38 +0300

    OOP 2_2_9 Не работает get_length

--- OOP/2/2_2_9.py
+++ OOP/2/2_2_9.py
@@ -25,9 +25,20 @@ class PathLines:
     def get_path(self):
         return self.coords
 
-    def get_length(self):
-        passL = sqrt((x1-x0)^2 + (y1-y0)^2)
+    def get_length(self): ##((x1-x0)**2 + (y1-y0)**2)**0,5
+        summa = []
+        l = 0
+        while l + 1 < len(self.coords):
+            value = ((self.coords[1 + l].x - self.coords[0 + l].x) ** 2 + (self.coords[1 + l].x - self.coords[0 + l].x) ** 2) ** 0, 5
+            l += 1
 
-    def dd_line(self, line):
+            summa.append(value)
+        return sum(summa)
+
+
+    def add_line(self, line):
         self.coords.append(line)
 
+p = PathLines(LineTo(10, 20), LineTo(10, 30))
+p.add_line(LineTo(20, -10))
+dist = p.get_length()
