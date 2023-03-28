cde1b0bdd0acbdaf94e811b2eb7c359977e0ca77
BUKOV <vaz21081@ua.fm>
2023-03-28 20:34:36 +0300

    OOP 2_3_6 начало1

---
+++ OOP/2/2_3_6_(descriptor).py
@@ -0,0 +1,29 @@
+class FloatValue:
+    @classmethod
+    def floatValue(cls, value):
+        if type(value) != float:
+            raise TypeError("Присваивать можно только вещественный тип данных.")
+
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if FloatValue.floatValue(value):
+            setattr(instance, self.name, value)
+
+
+class Cell:
+    value = FloatValue()
+
+    def __init__(self, value=0.0):
+        self.value = value
+
+
+class TableSheet:
+    def __init__(self, N, M):
+        self.line = N
+        self.column = M
+        cells = []
--- OOP/____.py
+++
@@ -1,8 +0,0 @@
-import  re
-
-def dddd(number):
-    pattern = r"\d\d\d\d-\d\d\d\d-\d\d\d\d-\d\d\d\d"
-    match = re.fullmatch(pattern, number)
-    print(True if match else False)
-
-dddd("7777-0000-5650-2333")
