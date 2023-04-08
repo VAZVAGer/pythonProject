6c09d638a83be2cc93a5be225cbaca719707e5df
BUKOV <vaz21081@ua.fm>
2023-04-08 22:13:35 +0300

    OOP 2_3_9 чет там навоял

---
+++ OOP/2/2_3_9.py
@@ -0,0 +1,25 @@
+class Bag:
+    def __init__(self, max_weight):
+        self.max_weight = max_weight
+        self.__things = []
+        self.weight = 0
+
+    @property
+    def things(self):
+        return self.__things
+
+    def add_thing(self, thing):
+        if self.thing.weight + self.weight < self.max_weight:
+            self.__things.append(thing)
+            self.weight = self.weight + self.thing.weight
+
+    def remove_thing(self, indx):
+        self.__things.pop(indx)
+
+    def get_total_weight(self):
+        return self.weight
+
+class Thing:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
