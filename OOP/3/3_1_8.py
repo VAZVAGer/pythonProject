345ac8a056da59829ef4ffd8f227de328f56691c
BUKOV <vaz21081@ua.fm>
2023-04-14 23:21:59 +0300

    OOP 3_1_8 как и следовало ожидать, не работает

---
+++ OOP/3/3_1_8.py
@@ -0,0 +1,69 @@
+class Circle:
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def __setattr__(self, key, value):
+        if key in ('__x', "__y"):
+            if key in (int, float):
+                object.__setattr__(self, key, value)
+        if key == "radius" and value > 0:
+            object.__setattr__(self, key, value)
+    def __delattr__(self, item):
+        return False
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
+
+    @property
+    def radius(self):
+        return self.__x
+
+    @radius.setter
+    def radius(self, radius):
+        self.__radius = radius
+
+
+assert type(Circle.x) == property and type(Circle.y) == property and type(
+    Circle.radius) == property, "в классе Circle должны быть объявлены объекты-свойства x, y и radius"
+
+try:
+    cr = Circle(20, '7', 22)
+except TypeError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение TypeError при инициализации объекта с недопустимыми аргументами"
+
+cr = Circle(20, 7, 22)
+assert cr.x == 20 and cr.y == 7 and cr.radius == 22, "объекты-свойства x, y и radius вернули неверные значения"
+
+cr.radius = -10  # прежнее значение не должно меняться, т.к. отрицательный радиус недопустим
+assert cr.radius == 22, "при присваивании некорректного значения, прежнее значение изменилось"
+
+x, y = cr.x, cr.y
+assert x == 20 and y == 7, "объекты-свойства x, y вернули некорректные значения"
+assert cr.name == False, "при обращении к несуществующему атрибуту должно возвращаться значение False"
+
+try:
+    cr.x = '20'
+except TypeError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение TypeError"
+
+cr.y = 7.8
+cr.radius = 10.6
