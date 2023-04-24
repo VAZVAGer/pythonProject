6371668f229020ae782d1132b9c1c017b1e8cedd
BUKOV <vaz21081@ua.fm>
2023-04-24 19:40:22 +0300

    OOP 3_2_2 работает как надо с первого раза

---
+++ OOP/3/3_2_2.py
@@ -0,0 +1,18 @@
+import random
+
+
+class RandomPassword:
+    def __init__(self, psw_chars, min_length, max_length):
+        self.psw_chars = psw_chars
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args, **kwargs):
+        password_size = random.randint(self.min_length, self.max_length)
+        password = random.sample(list(self.psw_chars), password_size)
+        return "".join(password)
+
+
+rnd = RandomPassword("qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*", 5, 20)
+lst_pass = [rnd() for _ in range(3)]
+print(lst_pass)
