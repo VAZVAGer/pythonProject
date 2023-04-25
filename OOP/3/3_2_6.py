acabb6d7f36307f328cb920e0a1e1975562af045
BUKOV <vaz21081@ua.fm>
2023-04-25 15:52:12 +0300

    OOP 3_2_6 ось тепер робить

--- OOP/3/3_2_6.py
+++ OOP/3/3_2_6.py
@@ -1,9 +1,12 @@
 class RenderList:
-    def __init__(self, type_list='ul'):
-        self.type_list = type_list
+    def __init__(self, type_list):
+        if type_list in ("ul", "ol"):
+            self.type_list = type_list
+        else:
+            self.type_list = "ul"
 
     def __call__(self, lst, *args, **kwargs):
-        return "\n".join([f'<{self.type_list}>', *map(lambda x: f'<li>{x}</li>', lst ), f'</{self.type_list}>'])
+        return "\n".join([f'<{self.type_list}>', *map(lambda x: f'<li>{x}</li>', lst), f'</{self.type_list}>'])
 
 
 lst = ["Пункт меню 1", "Пункт меню 2", "Пункт меню 3"]
