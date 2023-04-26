fe2692765bb83bcb6880337128220195bff2e933
BUKOV <vaz21081@ua.fm>
2023-04-26 21:13:59 +0300

    OOP 3_2_7 ось тепер робить, але я списав((((

---
+++ OOP/3/3_2_7.py
@@ -0,0 +1,28 @@
+class HandlerGET:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, request, *args, **kwargs):
+        m = request.get("method", "GET")
+        if m == "GET":
+            return self.get(self.func, request)
+        return None
+
+
+    def get(self, func, request, *args, **kwargs):
+        return f'GET: {func(request)}'
+
+@HandlerGET
+def index(request):
+    return "главная страница сайта"
+
+
+res = index({"method": "GET"})
+assert res == "GET: главная страница сайта", "декорированная функция вернула неверные данные"
+res = index({"method": "POST"})
+assert res is None, "декорированная функция вернула неверные данные"
+res = index({"method": "POST2"})
+assert res is None, "декорированная функция вернула неверные данные"
+
+res = index({})
+assert res == "GET: главная страница сайта", "декорированная функция вернула неверные данные"
