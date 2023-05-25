97b6845b94c53a7f1ff7f03fb83032aaccc5fbbe
BUKOV <vaz21081@ua.fm>
2023-05-25 22:24:10 +0300

    OOP 3_3_5  готово

--- OOP/3/3_3_6.py
+++ OOP/3/3_3_6.py
@@ -9,16 +9,24 @@ class Complex:
 
     @real.setter
     def real(self, val):
-        if val in (int, float):
-            self.__real = val
+        if type(val) not in (int, float):
+            raise ValueError("Неверный тип данных.")
+        self.__real = val
     @property
     def img(self):
         return self.__img
 
     @img.setter
     def img(self, val):
-        if val in (int, float):
-            self.__img = val
+        if type(val) not in (int, float):
+            raise ValueError("Неверный тип данных.")
+        self.__img = val
 
     def __abs__(self):
-        return abs((self.real * self.real + self.img * self.img)**0.5)
+        return (self.__real * self.__real + self.__img * self.__img)**0.5
+
+cmp = Complex(7, 8)
+cmp.real = 3
+cmp.img = 4
+c_abs = abs(cmp)
+print(c_abs)
