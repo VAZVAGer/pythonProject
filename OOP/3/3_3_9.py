24dada5e35359576b5bd2774ce1ad07fe6096555
BUKOV <vaz21081@ua.fm>
2023-05-30 22:17:55 +0300

    OOP 3_3_9 в процессе

---
+++ OOP/3/3_3_9.py
@@ -0,0 +1,12 @@
+class Recipe:
+    def __init__(self, *args):
+        self._recipe = args
+
+class Ingredient:
+    def __init__(self, name, volume, measure):
+        self.name = name
+        self.volume = volume
+        self.measure = measure
+
+    def __str__(self):
+        return f"{self.name}: {self.volume}, {self.measure}."
