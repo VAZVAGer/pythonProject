e02ed51dd584635bf0d3458097a4bbc07c4e69f8
BUKOV <vaz21081@ua.fm>
2023-05-30 22:33:51 +0300

    OOP 3_3_9 готово

--- OOP/3/3_3_9.py
+++ OOP/3/3_3_9.py
@@ -2,6 +2,9 @@ class Recipe:
     def __init__(self, *args):
         self._recipe = list(args)
 
+    def __len__(self):
+        return len(self._recipe)
+
     def add_ingredient(self, ing):
         self._recipe.append(ing)
 
@@ -19,4 +22,24 @@ class Ingredient:
         self.measure = measure
 
     def __str__(self):
-        return f"{self.name}: {self.volume}, {self.measure}."
+        return f"{self.name}: {self.volume}, {self.measure}"
+
+i1 = Ingredient("Соль", 1, "столовая ложка")
+i2 = Ingredient("Мука", 1, "кг")
+i3 = Ingredient("Мясо баранины", 10, "кг")
+i4 = Ingredient("Масло", 100, "гр")
+recipe = Recipe(i1, i2, i3)
+recipe.add_ingredient(i4)
+recipe.remove_ingredient(i3)
+
+assert len(recipe) == 3, "функция len вернула неверное значение"
+lst = recipe.get_ingredients()
+for x in lst:
+    assert isinstance(x, Ingredient), "в списке рецептов должны быть только объекты класса Ingredient"
+    assert hasattr(x, 'name') and hasattr(x, 'volume') and hasattr(x, 'measure'), "объект класса Ingredient должен иметь атрибуты: name, volume, measure"
+
+assert str(i4) == "Масло: 100, гр", "метод __str__ вернул неверное значение"
+
+i4 = Ingredient("Масло", 120, "гр")
+recipe.add_ingredient(i4)
+assert len(recipe) == 4, "функция len вернула неверное значение"
