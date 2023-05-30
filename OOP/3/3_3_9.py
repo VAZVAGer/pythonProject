ac4f0086e6a311304c52554c8dcacc862cb6c48b
BUKOV <vaz21081@ua.fm>
2023-05-30 22:26:49 +0300

    OOP 3_3_9 в процессе2

--- OOP/3/3_3_9.py
+++ OOP/3/3_3_9.py
@@ -1,6 +1,16 @@
 class Recipe:
     def __init__(self, *args):
-        self._recipe = args
+        self._recipe = list(args)
+
+    def add_ingredient(self, ing):
+        self._recipe.append(ing)
+
+    def remove_ingredient(self, ing):
+        self._recipe.remove(ing)
+
+    def get_ingredients(self):
+        return tuple(self._recipe)
+
 
 class Ingredient:
     def __init__(self, name, volume, measure):
