3b450307ca50f0674d4849695338f497e9c8616d
BUKOV <vaz21081@ua.fm>
2023-06-23 21:54:28 +0300

    OOP 3_4_6 готово

--- OOP/3/3_4_6.py
+++ OOP/3/3_4_6.py
@@ -41,3 +41,38 @@ class Stack:
         self.end = counter
         counter.next = None
 
+    def __add__(self, other):
+        self.push_back(other)
+        return self
+
+    def __mul__(self, other):
+        for i in other:
+            self.push_back(StackObj(i))
+        return self
+
+
+assert hasattr(Stack, 'pop_back'), "класс Stack должен иметь метод pop_back"
+
+st = Stack()
+top = StackObj("1")
+st.push_back(top)
+assert st.top == top, "неверное значение атрибута top"
+
+st = st + StackObj("2")
+st = st + StackObj("3")
+obj = StackObj("4")
+st += obj
+
+st = st * ['data_1', 'data_2']
+st *= ['data_3', 'data_4']
+
+d = ["1", "2", "3", "4", 'data_1', 'data_2', 'data_3', 'data_4']
+h = top
+i = 0
+while h:
+    assert h._StackObj__data == d[
+        i], "неверное значение атрибута __data, возможно, некорректно работают операторы + и *"
+    h = h._StackObj__next
+    i += 1
+
+assert i == len(d), "неверное число объектов в стеке"
