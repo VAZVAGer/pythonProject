fde9b943ff236c01e6b32c4ba338d5af5409bbaa
BUKOV <vaz21081@ua.fm>
2023-07-17 22:29:32 +0300

    OOP 3_5_3 в процессе

--- OOP/3/3_5_3.py
+++ OOP/3/3_5_3.py
@@ -8,8 +8,25 @@ class Track:
     def get_tracks(self):
         return self.track
 
+    def __len__(self):
+        if len(self.track) < 2:
+            return 0
+        l_e_n = 0
+        for i in range(len(self.track) - 1):
+            print(self.track[i:i + 2])
+            time_t = (self.track[i:i + 2])
+            l_e_n += ((time_t[-1][0] - time_t[0][0]) ** 2 + (time_t[-1][-1] - time_t[0][-1]) ** 2)
+        return l_e_n
 
 
 class TrackLine:
     def __init__(self, to_x, to_y, max_speed):
         self.trackline = ((to_x, to_y, max_speed),)
+
+# track1 = Track(0, 0)
+# track1.add_track(TrackLine(2, 4, 100))
+# track1.add_track(TrackLine(5, -4, 100))
+#
+# track2 = Track(0, 1)
+# track2.add_track(TrackLine(3, 2, 90))
+# track2.add_track(TrackLine(10, 8, 90))
--- OOP/____.py
+++ OOP/____.py
@@ -1,4 +1,11 @@
 a = ((1, 2),)
 b = ((7, 9),)
-a = a + b
+c = ((8, 33),)
+a = a + b + c
 print(a)
+l = 0
+for i in range(len(a)-1):
+    print(a[i:i+2])
+    time_t = (a[i:i+2])
+    l += ((time_t[-1][0] - time_t[0][0])**2 + (time_t[-1][-1] - time_t[0][-1])**2)
+print(l)
