bb7436309c9c0bbf3a06d2d7c7692d7cfbc29176
BUKOV <vaz21081@ua.fm>
2023-09-27 21:57:39 +0300

    OOP 3_8_9  готово

--- OOP/3/3_8_9.py
+++ OOP/3/3_8_9.py
@@ -11,10 +11,43 @@ class Bag:
         self.weight_bag = 0
 
     def add_thing(self, thing):
-        if self.weight_bag + thing.weight > self.__max_weight:
+        if (self.weight_bag + thing.weight) > self.__max_weight:
             raise ValueError('превышен суммарный вес предметов')
         self.bag.append(thing)
         self.weight_bag += thing.weight
 
+    def chack(self, ind):
+        if ind < 0 or ind > len(self.bag) - 1:
+            raise IndexError('неверный индекс')
+
+    def __getitem__(self, item):
+        self.chack(item)
+        return self.bag[item]
+
+    def __setitem__(self, key, value):
+        old_object = self[key]
+        if (self.weight_bag - old_object.weight + value.weight) > self.__max_weight:
+            raise ValueError('превышен суммарный вес предметов')
+        self.weight_bag = self.weight_bag - old_object.weight + value.weight
+        self.chack(key)
+        self.bag[key] = value
 
 
+    def __delitem__(self, key):
+        del self.bag[key]
+
+
+
+
+b = Bag(700)
+b.add_thing(Thing('книга', 100))
+b.add_thing(Thing('носки', 200))
+
+b[0] = Thing('рубашка', 500)
+
+try:
+    b[0] = Thing('рубашка', 800)
+except ValueError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение ValueError при замене предмета в объекте класса Bag по индексу"
