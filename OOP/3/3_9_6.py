24d2237c31c4b1958a065c412aa1b7783e3d2399
BUKOV <vaz21081@ua.fm>
2023-10-12 23:01:40 +0300

    OOP 3_9_6  мучаю

--- OOP/3/3_9_6.py
+++ OOP/3/3_9_6.py
@@ -1,11 +1,29 @@
 class TriangleListIterator:
     def __init__(self, lst):
-        self._lst = lst
+        self.lst = lst
+        self.row = 0
+        self.element = 1
+
 
     def __iter__(self):
-        for i in range(len(self._lst)):
-            for j in range(i + 1):
-                yield self._lst[i][j]
+        return self
+
+    def __next__(self):
+        if self.row <= len(self.lst) - 1:
+            self.line = self.lst[self.row][:self.element]
+            self.row += 1
+            self.element += 1
+            return self.line
+        else:
+            raise StopIteration
+
+
+
+
+
+
+
+
 lst = [[1, 11, 111, 1111, 11111, 111111],
        [2, 22, 222, 2222, 22222, 222222],
        [3, 33, 333, 3333, 33333, 333333],
--- OOP/___________.py
+++ OOP/___________.py
@@ -1,5 +1,2 @@
-a = [[]]
-a[0].append(2)
-print(a[0][0])
-a[0][1] = 7
-print(a[0][0])
+D = []
+print(dir(D))
