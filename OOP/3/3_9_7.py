29b11e1a5b0d3294e131cc0872e5d1984b0e1d6c
BUKOV <vaz21081@ua.fm>
2023-10-16 21:31:45 +0300

    OOP 3_9_7  готово

--- OOP/3/3_9_7.py
+++ OOP/3/3_9_7.py
@@ -1,31 +1,35 @@
-class TriangleListIterator:
-    def __init__(self, lst):
+class IterColumn:
+    def __init__(self, lst, column):
         self.lst = lst
+        self.column = column
+
 
     def __iter__(self):
         self.row = 0
-        self.element = 0
         return self
 
     def __next__(self):
-        if self.row == self.element:
-            value = self.lst[self.row][self.element]
+        if self.row < len(self.lst):
+            value = self.lst[self.row][self.column]
             self.row += 1
-            self.element = 0
             return value
-        elif self.row > self.element and self.row < len(self.lst):
-            value1 = self.lst[self.row][self.element]
-            self.element += 1
-            return value1
 
         else:
             raise StopIteration
 
 
-lst = [[1, 11, 111, 1111, 11111, 111111],
+lst = [[1, 11, 111],
        [2, 22, 222, 2222, 22222, 222222],
        [3, 33, 333, 3333, 33333, 333333],
        [4, 44, 444, 4444, 44444, 444444]]
-it = TriangleListIterator(lst)
+it = IterColumn(lst, 2)
 for x in it:  # последовательный перебор всех элементов списка: x00, x10, x11, x20, ...
     print(x)
+it_iter = iter(it)
+# x = next(it_iter)
+#
+# print(next(it_iter))
+# print(next(it_iter))
+# print(next(it_iter))
+# print(next(it_iter))
+#
