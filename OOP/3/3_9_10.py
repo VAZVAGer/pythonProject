0eea4e2dfcd0d3d25734d4fbe9be167bf3655088
BUKOV <vaz21081@ua.fm>
2023-11-24 22:10:22 +0200

    OOP 3_9_10 что-то там наваял, надеюсь верно

--- OOP/3/3_9_10.py
+++ OOP/3/3_9_10.py
@@ -1,4 +1,13 @@
 class Matrix:
+    def cheack_list2D(self, list):
+        for line in list:
+            if len(line) != len(list):
+                raise TypeError('список должен быть прямоугольным, состоящим из чисел')
+            for element in list:
+                if not isinstance(element, (int, float)):
+                    raise TypeError('список должен быть прямоугольным, состоящим из чисел')
+
+
     def __init__(self, *args):
         if len(args) == 3:
             if type(args[0]) != int or type(args[1]) != int or isinstance(args[2], (int, float)):
@@ -7,4 +16,17 @@ class Matrix:
             self.__cols = args[1]
             self.__matriX = list(list(args[2] for _ in range(self.__cols)) for _ in range(self.__rows))
         elif len(args) == 1:
-            self.__matriX = args
+            self.cheack_list2D(args)
+            self.__matriX = [args]
+
+    def __getitem__(self, item):
+        ind1, ind2 = item
+        if 0 > ind1 > len(self.__matriX) or 0 > ind2 > len(self.__matriX[ind1]):
+            raise IndexError('недопустимые значения индексов')
+        return self.__matriX[ind1][ind2]
+
+    def __setitem__(self, key, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError('значения матрицы должны быть числами')
+        ind1, ind2 = key
+        self.__matriX[ind1][ind2] = value
