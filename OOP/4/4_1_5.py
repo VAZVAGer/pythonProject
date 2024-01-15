f4909f74a617d6c0063f83f2e10fc9a35d7163e3
BUKOV <vaz21081@ua.fm>
2024-01-15 21:42:25 +0200

    OOP 4_1_5  вроде сделал

---
+++ OOP/4/4_1_5.py
@@ -0,0 +1,25 @@
+class Thing:
+    counter = 0
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        self.id = Thing.counter
+        Thing.counter += 1
+        self.weight = None
+        self.dims = None
+        self.memory = None
+        self.frm = None
+
+    def get_data(self):
+        return tuple(self.id, self.name, self.price, self.weight, self.dims, self.memory, self.frm)
+
+
+class Table(Thing):
+    def __init__(self, name, price, weight, dims):
+        super().__init__(name, price, weight, dims)
+
+
+class ElBook(Thing):
+    def __init__(self, name, price, memory, frm):
+        super().__init__(name, price, memory, frm)
