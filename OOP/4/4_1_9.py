fa39c3d29115df6cdcb01e1c89365eb4019b8760
BUKOV <vaz21081@ua.fm>
2024-01-23 20:47:32 +0200

    OOP 4_1_8 готово

---
+++ OOP/4/4_1_9.py
@@ -0,0 +1,45 @@
+class Layer:
+    def __init__(self):
+        self.next_layer = None
+        self.name = 'Layer'
+
+    def __call__(self, leyer, *args, **kwargs):
+        self.next_layer = leyer
+        return leyer
+class Input(Layer):
+    def __init__(self, inputs):
+        super().__init__()
+        self.inputs = inputs
+        self.name = 'Input'
+
+class Dense(Layer):
+    def __init__(self, inputs, outputs, activation):
+        super().__init__()
+        self.name = 'Dense'
+        self.inputs = inputs
+        self.outputs = outputs
+        self.activation = activation
+
+class NetworkIterator:
+    def __init__(self, data):
+        self.data = data
+
+    def __iter__(self):
+        layer = self.data
+        while layer:
+            yield layer
+            layer = layer.next_layer
+
+
+
+nt = Input(12)
+layer = nt(Dense(nt.inputs, 1024, 'relu'))
+layer = layer(Dense(layer.inputs, 2048, 'relu'))
+layer = layer(Dense(layer.inputs, 10, 'softmax'))
+
+n = 0
+for x in NetworkIterator(nt):
+    assert isinstance(x, Layer), "итератор должен возвращать объекты слоев с базовым классом Layer"
+    n += 1
+
+assert n == 4, "итератор перебрал неверное число слоев"
