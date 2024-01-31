696e566b34211d5a782020c227a0492cc7b1f9fe
BUKOV <vaz21081@ua.fm>
2024-01-31 21:44:34 +0200

    OOP 4_2_5 готово

--- OOP/4/4_2_5.py
+++ OOP/4/4_2_5.py
@@ -32,11 +32,14 @@ class Human(Mammals):
 
 class Monkeys(Mammals):
     pass
+
+
 class Monkey(Monkeys):
     def __init__(self, name, weight, old):
         self.name = name
         self.weight = weight
         self.old = old
+
 
 class Person(Human):
     def __init__(self, name, weight, old):
@@ -51,6 +54,7 @@ class Flower(Flowering):
         self.weight = weight
         self.old = old
 
+
 class Worm(Worms):
     def __init__(self, name, weight, old):
         self.name = name
@@ -58,4 +62,11 @@ class Worm(Worms):
         self.old = old
 
 
+lst_objs = [Monkey("мартышка", 30.4, 7), Monkey("шимпанзе", 24.6, 8),
+            Person("Балакирев", 88, 34), Person("Верховный жрец", 67.5, 45),
+            Flower("Тюльпан", 0.2, 1), Flower("Роза", 0.1, 2),
+            Worm("червь", 0.01, 1), Worm("червь 2", 0.02, 1)]
 
+lst_animals = [x for x in lst_objs if isinstance(x, Animals)]  ## все объекты, относящиеся к животным (Animals);
+lst_plants = [x for x in lst_objs if isinstance(x, Plants)]  ## все объекты, относящиеся к растениям (Plants);
+lst_mammals = [x for x in lst_objs if isinstance(x, Mammals)]  ## все объекты, относящиеся к млекопитающим (Mammals).
