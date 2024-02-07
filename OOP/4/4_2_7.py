7b61eb42fb611c46719a515acf2892bb176f60df
BUKOV <vaz21081@ua.fm>
2024-02-07 21:36:59 +0200

    OOP 4_2_7 готово

---
+++ OOP/4/4_2_7.py
@@ -0,0 +1,29 @@
+class VideoItem:
+    def __init__(self, title, descr, path):
+        self.title = title
+        self.descr = descr
+        self.path = path
+        self.rating = VideoRating()
+
+class VideoRating:
+    def __init__(self, value=0):
+        self.__rating = value
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value):
+        if type (value) != int or not 0 <= value <= 5:
+            raise ValueError('неверное присваиваемое значение')
+        self.__rating = value
+
+
+v = VideoItem('Курс по Python ООП', 'Подробный курс по Python ООР', 'D:/videos/python_oop.mp4')
+print(v.rating.rating) # 0
+v.rating.rating = 2
+print(v.rating.rating) # 5
+title = v.title
+descr = v.descr
+v.rating.rating = 6  # ValueError
