2dcf34604f71f27bf00d9557774571128e250c75
BUKOV <vaz21081@ua.fm>
2024-02-12 21:22:08 +0200

    OOP 4_3_5 готово

--- OOP/4/4_3_5.py
+++ OOP/4/4_3_5.py
@@ -1,7 +1,8 @@
 class SellItem:
-    def __iter__(self, name, price):
+    def __init__(self, name, price):
         self.name = name
         self.price = price
+
 
 class House(SellItem):
     def __init__(self, name, price, material, square):
@@ -16,7 +17,35 @@ class Flat(SellItem):
         self.size = size
         self.rooms = rooms
 
+
 class Land(SellItem):
-    def __init__(self,name, price, square):
+    def __init__(self, name, price, square):
         super().__init__(name, price)
         self.square = square
+
+
+class Agency:
+    def __init__(self, name):
+        self.name = name
+        self.list_obj = []
+
+    def add_object(self, obj):
+        self.list_obj.append(obj)
+
+    def remove_object(self, obj):
+        self.list_obj.remove(obj)
+
+    def get_objects(self):
+        return self.list_obj
+
+
+ag = Agency("Рога и копыта")
+ag.add_object(Flat("квартира, 3к", 10000000, 121.5, 3))
+ag.add_object(Flat("квартира, 2к", 8000000, 74.5, 2))
+ag.add_object(Flat("квартира, 1к", 4000000, 54, 1))
+ag.add_object(House("дом, крипичный", price=35000000, material="кирпич", square=186.5))
+ag.add_object(Land("участок под застройку", 3000000, 6.74))
+for obj in ag.get_objects():
+    print(obj.name)
+
+lst_houses = [x for x in ag.get_objects() if isinstance(x, House)] # выделение списка домов
