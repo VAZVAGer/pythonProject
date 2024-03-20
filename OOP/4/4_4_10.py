60dad8da3d5883116c7b00ab18062bfabaee3d38
BUKOV <vaz21081@ua.fm>
2024-03-20 21:22:08 +0200

    OOP 4_4_10 готово

---
+++ OOP/4/4_4_10.py
@@ -0,0 +1,36 @@
+CURRENT_OS = 'windows'  # 'windows', 'linux'
+
+
+class WindowsFileDialog:
+    def __init__(self, title, path, exts):
+        self.__title = title  # заголовок диалогового окна
+        self.__path = path  # начальный каталог с файлами
+        self.__exts = exts  # кортеж из отображаемых расширений файлов
+
+
+class LinuxFileDialog:
+    def __init__(self, title, path, exts):
+        self.__title = title  # заголовок диалогового окна
+        self.__path = path  # начальный каталог с файлами
+        self.__exts = exts  # кортеж из отображаемых расширений файлов
+
+
+class FileDialogFactory:
+    @staticmethod
+    def create_windows_filedialog(title, path, exts):
+        return WindowsFileDialog(title, path, exts)
+
+    @staticmethod
+    def create_linux_filedialog(title, path, exts):
+        return LinuxFileDialog(title, path, exts)
+
+    def __new__(cls, title, path, exts):
+        if CURRENT_OS == 'windows':
+            return cls.create_windows_filedialog(title, path, exts)
+        if CURRENT_OS == 'linux':
+            return cls.create_linux_filedialog(title, path, exts)
+
+
+
+dlg = FileDialogFactory('Изображения', 'd:/images/', ('jpg', 'gif', 'bmp', 'png'))
+print(type(dlg))
